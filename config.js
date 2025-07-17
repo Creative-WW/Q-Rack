258c4ce2183e29b5b5861391b259c39d35129e6a
Creative-WW <creative@wwdcomplete.com>
2025-07-17 14:53:54 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // config.js
 window.projectConfig = {
-  title: "DQ3358 . Q Rack",
-  description: "View the Q Rack in AR on your mobile device.",
-  image: "assets/Q Rack.png",
-  usdz: "assets/AR_DQ3358.usdz", // relative path works on GitHub Pages
-  glb: "https://creative-ww.github.io/Q-Rack/assets/AR_DQ3358.glb", // FULL HTTPS URL to GLB on GitHub Pages
+  title: "DQ8664 . Checkout Bunker",
+  description: "View the Checkout Bunker in AR on your mobile device.",
+  image: "assets/Checkout Rack.png",
+  usdz: "assets/AR_DQ8664.usdz", // relative path works on GitHub Pages
+  glb: "https://creative-ww.github.io/Q-Rack/assets/AR_DQ8664.glb", // FULL HTTPS URL to GLB on GitHub Pages
   logo: "assets/dq_logo.png"
 };
