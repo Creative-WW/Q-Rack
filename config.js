71269abce5e26332f0d1e9baa26bd6238055cf7b
Creative-WW <creative@wwdcomplete.com>
2025-07-17 12:06:21 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ window.projectConfig = {
   description: "View the Q Rack in AR on your mobile device.",
   image: "assets/Q Rack.png", // used for AR button
   usdz: "assets/AR_DQ3358.usdz",
+  glb: "https://creative-ww.github.io/assets/AR_DQ3358.glb", 
   glb: "assets/AR_DQ3358.glb",
   logo: "assets/dq_logo.png" // company logo
 };
